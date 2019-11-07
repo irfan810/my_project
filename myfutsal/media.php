a1f166f351a240548f07e89acb2d34486e1679ee
irfan810 <irfan.maulana810@gmail.com>
2019-11-07 05:06:58 +0000

    "menghapus file"

--- myfutsal/media.php
+++
@@ -1,8 +0,0 @@
-<?php 
-include "config/koneksi.php";
-include "config/fungsi_indotgl.php";
-include "config/class_paging.php";
-include "config/library.php"; 
-include "config/fungsi_thumbuser.php";
-include "page/index.php"; 
-?>
